ea93ec56bf26556b87f735b9d09aa5dc2850b052
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-16 16:10:58 +0900

    シェル修正

--- com.sh
+++ com.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-babel input/$1/$2 --out-file output/$1/$2
+babel input/$1/$2.js --out-file output/$1/$2.js
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-osascript -l JavaScript ./input/$1/$2
+osascript -l JavaScript ./input/$1/$2.js
