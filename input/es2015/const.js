da4cff12bba24a335e88c498a057f3a43bee946a
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-16 16:10:08 +0900

    const

---
+++ input/es2015/const.js
@@ -0,0 +1,7 @@
+// constによる変数宣言と初期化
+const FOO = 'foo'
+console.log(FOO)
+
+// 再代入
+// FOO = 'FOO' // error
+// console.log(FOO)
---
+++ output/es2015/const.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// constによる変数宣言と初期化
+var FOO = 'foo';
+console.log(FOO);
+
+// 再代入
+// FOO = 'FOO' // error
+// console.log(FOO)
