e0e48c5cdb7c1ba2d2411e113c7e654f779b386d
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-16 14:44:28 +0900

    varンいよる変数宣言を記述

---
+++ input/es2015/let.js
@@ -0,0 +1,11 @@
+// varによる従来の変数宣言と初期化
+var foo = 'foo'
+console.log(foo)    // foo
+
+// 再代入
+foo = 'FOO'
+console.log(foo)    // FOO
+
+// 再宣言
+var foo = 'foo'
+console.log(foo)    // foo
--- output/es2015/001_let.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-var hoge = "a";
