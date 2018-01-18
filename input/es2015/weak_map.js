ea7dbf1502cfc99b119e88b896ea3167084d30a4
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-18 16:04:37 +0900

    WeakMap, Map修正

--- input/es2015/weak_map.js
+++ input/es2015/weak_map.js
@@ -2,8 +2,10 @@
 // キーによるオブジェクトの参照がMapよりも弱く保持され, オブジェクトへの参照が存在しない場合はガベージコレクション(GC)の対象となる
 // そのため, キーが列挙可能ではない(すなわち, キーのリストを取得するメソッドがない)
 const weakmap = new WeakMap()
+
 weakmap.set({}, 'foo')
 weakmap.set({}, 'bar')
 weakmap.set({}, 'baz')
 
 console.log(weakmap)
+console.log(weakmap.get({}))
