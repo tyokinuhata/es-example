d3f9b2d4f21ef5d33cb7549ae1dfb607a71fe887
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-18 14:27:22 +0900

    Map修正

--- input/es2015/map.js
+++ input/es2015/map.js
@@ -1,8 +1,10 @@
 // Map
 // 連想配列のようなもの
 const map = new Map()
+
 map.set('foo', 'foo')
 map.set('bar', 'bar')
 map.set('baz', 'baz')
 
 console.log(map)    // Map { 'foo' => 'foo', 'bar' => 'bar', 'baz' => 'baz' }
+console.log(map.get('foo')) // foo
