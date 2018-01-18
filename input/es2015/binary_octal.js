149a98e65b9bbc9b070dc7e496420b312fc353af
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-18 11:37:17 +0900

    ２進数・８進数リテラル

---
+++ input/es2015/binary_octal.js
@@ -0,0 +1,17 @@
+// ２進数
+const bin = 0b0001
+
+// ８進数
+const oct = 0o02
+
+console.log(bin)
+console.log(oct)
+
+// 10進数
+const dec = 3
+
+// 16進数
+const hex = 0x04
+
+console.log(dec)
+console.log(hex)
