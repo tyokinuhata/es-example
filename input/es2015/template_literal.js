c576c95bc785cfb74c8b76617003124537f991a4
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-18 10:13:07 +0900

    テンプレート文字列

---
+++ input/es2015/template_literal.js
@@ -0,0 +1,16 @@
+const name = 'hoge'
+
+// シングルクォーテーション
+console.log('My name is ' + name)
+
+// ダブルクォーテーション
+console.log("My name is " + name)
+
+// テンプレート文字列
+console.log(`My name is ${name}`)
+
+// 複数行のテンプレート文字列
+console.log(`My
+name
+is
+${name}`)
