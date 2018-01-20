037c8d9589b47bcc6d04fac1ec6cfec91a4fab25
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-21 04:16:20 +0900

    Proxy

---
+++ input/es2015/proxy.js
@@ -0,0 +1,19 @@
+const target = {}
+const handler = {
+    set: (target, name, value) => {
+        if (typeof value === 'number') {
+            value *= 2
+        }
+        target[name] = value
+    }
+}
+
+const proxy = new Proxy(target, handler)
+
+proxy.foo = 100
+proxy.bar = 200
+proxy.baz = "200"
+
+console.log(proxy.foo)
+console.log(proxy.bar)
+console.log(proxy.baz)
