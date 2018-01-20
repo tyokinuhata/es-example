6827f7c81e2eab18a82ad723f432edb4219b73db
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-21 05:14:59 +0900

    イテレータ

---
+++ input/es2015/iterator.js
@@ -0,0 +1,29 @@
+// イテラブル
+// イテレータを持つオブジェクト
+// Symbol.iterator()メソッドを実行すると, イテレータを返す
+const obj = {}
+obj[Symbol.iterator] = () => {
+    // イテレータ
+    // next()メソッドを持ち, 実行するとリテレータリザルトを返すもの
+    const iterator = {}
+    let count = 1
+    iterator.next = () => {
+        // イテレータリザルト
+        // 取り出した値, 取り出したかどうかの真偽値を持つオブジェクト
+        const iteratorResult = count <= 10
+            ? {
+                value: count++,
+                done: false
+            }
+            : {
+                value: undefined,
+                done: true
+            }
+        return iteratorResult
+    }
+    return iterator
+}
+
+for (const value of obj) {
+    console.log(value)
+}
